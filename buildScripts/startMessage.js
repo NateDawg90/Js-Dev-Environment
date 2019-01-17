d38b9dbb8b8b98606e735b62d71220b32f781b38
Nathan Johnson <rnj@clemson.edu>
2019-01-17 15:55:17 -0600

    add eslintrc and lint script; add test script and first mocha test

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,3 @@
-// var chalk = require('chalk');
 import chalk from 'chalk';
 
-console.log(chalk.green('Running the application in dev mode...'));
+console.log(chalk.green('Running the application in dev mode...'));  // eslint-disable-line no-console
