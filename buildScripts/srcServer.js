deed64e923415f05e11ede02ded6a850ec721076
Nathan Johnson <rnj@clemson.edu>
2019-01-18 14:40:14 -0600

    add static json for users

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1, "firstName": "Dan", "lastName": "Desotto", "email": "dandesotto@yahoo.com"},
+    {"id": 2, "firstName": "Nate", "lastName": "Johnson", "email": "rnj@gmail.com"},
+    {"id": 3, "firstName": "Creed", "lastName": "Johnson", "email": "creeddj@gmail.com"}
+  ])
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
